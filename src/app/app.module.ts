95b31b12e0972cdd12792e395c343aa0947f174d
Joseph Franco <josefraalv@gmail.com>
2024-02-16 18:41:28 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
+import { CenterBarComponent } from './components/center-bar/center-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideBarComponent,
+    CenterBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/center-bar/center-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'center-bar',
+  templateUrl: './center-bar.component.html',
+  styleUrls: ['./center-bar.component.css'],
+})
+export class CenterBarComponent {}
